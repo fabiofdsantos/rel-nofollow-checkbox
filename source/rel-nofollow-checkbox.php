ed6ad279cca551e67d7883b265bf7fd7a3d82931
Fábio Santos <ffsantos92@gmail.com>
2015-04-26 13:56:59 +0100

    Fix 'wpLinkL10n is not defined'

--- source/rel-nofollow-checkbox.php
+++ source/rel-nofollow-checkbox.php
@@ -5,14 +5,16 @@
  * Description: This plugin adds a simple checkbox in the insert/edit link popup for including nofollow attribute.
  * Version:     1.1.0
  * Author:      Fabio Santos
- * Author URI:  http://www.fabiosantos.me
+ * Author URI:  http://www.fabiosantos.me.
  */
-function overwrite_wplink() {
-	// Disable wplink
-	wp_deregister_script( 'wplink' );
+function overwrite_wplink()
+{
+    // Disable wplink
+    wp_deregister_script('wplink');
 
-	// Register a new script file to be linked
-	wp_register_script( 'wplink', plugins_url( 'wplink.min.js', __FILE__), array( 'jquery', 'wpdialogs' ), false, 1 );
+    // Register a new script file to be linked
+    wp_register_script('wplink', plugins_url('wplink.min.js', __FILE__), array( 'jquery', 'wpdialogs' ), false, 1);
+
+    wp_localize_script('wplink', 'wpLinkL10n', array('title' => __('Insert/edit link'), 'update' => __('Update'), 'save' => __('Add Link'), 'noTitle' => __('(no title)'), 'noMatchesFound' => __('No matches found.') ));
 }
-add_action( 'admin_enqueue_scripts', 'overwrite_wplink', 999 );
-?>
+add_action('admin_enqueue_scripts', 'overwrite_wplink', 999);
